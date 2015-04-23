1be5ecfa7cc2e2ddeed98c3d65e2ae2ae06453f5
davidchin <davidchin.yf@gmail.com>
2015-04-23 23:19:02 +1000

    Update remote repo address

--- Gruntfile.js
+++ Gruntfile.js
@@ -303,7 +303,7 @@ module.exports = function (grunt) {
     buildcontrol: {
       dist: {
         options: {
-          remote: 'git@github.com:davidchin/davidchin.git',
+          remote: 'git@github.com:davidchin/davidchin.github.io.git',
           branch: 'master',
           commit: true,
           push: true
