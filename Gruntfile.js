983d7b356e005ff348b8c275db0a99f9153367cb
davidchin <davidchin.yf@gmail.com>
2015-04-27 08:06:24 +1000

    Add icon

--- Gruntfile.js
+++ Gruntfile.js
@@ -285,11 +285,11 @@ module.exports = function (grunt) {
             'img/**/*',
             'fonts/**/*',
             // Like Jekyll, exclude files & folders prefixed with an underscore.
-            '!**/_*{,/**}'
+            '!**/_*{,/**}',
             // Explicitly add any files your site needs for distribution here.
             //'_bower_components/jquery/jquery.min.js',
-            //'favicon.ico',
-            //'apple-touch*.png'
+            'favicon.ico',
+            'apple-touch*.png'
           ],
           dest: '<%= yeoman.dist %>'
         },
